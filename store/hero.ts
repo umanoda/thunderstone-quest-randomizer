8bf119a5117b9c981f432027c88eca7387a88eb5
umanoda <hhenpitu@gmail.com>
2019-09-12 20:09:59 +0900

    impl heros randamizer

--- store/hero.ts
+++ store/hero.ts
@@ -1,44 +1,94 @@
 import { CardState, Card } from "~/types";
 import { MutationTree, ActionTree } from "vuex";
 
-const candidate = {
-  weapons: ["Short Sword**", "Mace**", "Long Sword**", "Halberd**", "Axe**", "Shortsear**"],
-  magics: ["Fireball**", "Feature**", "Ice Needle**", "Teleport**", "Magic Weapon**"],
-  items: ["Orb A**", "Orb B**", "Orb C**", "Orb D**", "Orb E**", "Orb F**"],
-};
+type Candidate = {
+  name: string;
+  tags: string[];
+}
+
+const candidates : Candidate[] = [
+  { name: "Hero 1**", tags: ["HUMAN", "FIGHTER"] },
+  { name: "Hero 2**", tags: ["HUMAN", "MAGICIAN"] },
+  { name: "Hero 3**", tags: ["HUMAN", "CLERIC"] },
+  { name: "Hero 4**", tags: ["HUMAN", "FIGHTER", "ROAGE"] },
+  { name: "Hero 5**", tags: ["HUMAN", "ROAGE"] },
+  { name: "Hero 6**", tags: ["HUMAN", "FIGHTER", "MAGICIAN"] },
+  { name: "Hero 7**", tags: ["HUMAN", "MAGICIAN", "CLERIC"] },
+  { name: "Hero 8**", tags: ["HUMAN", "FIGHTER"] },
+  { name: "Hero 9**", tags: ["HUMAN", "MAGICIAN"] },
+  { name: "Hero 10**", tags: ["HUMAN", "CLERIC"] },
+  { name: "Hero 11**", tags: ["HUMAN", "ROAGE"] },
+  { name: "Hero 12**", tags: ["HUMAN", "ROAGE", "MAGICIAN"] },
+  { name: "Hero 13**", tags: ["HUMAN", "ROAGE", "CLERIC"] },
+  { name: "Hero 14**", tags: ["HUMAN", "MAGICIAN"] },
+  { name: "Hero 15**", tags: ["HUMAN", "CLERIC", "MAGICIAN"] },
+  { name: "Hero 16**", tags: ["HUMAN", "FIGHTER", "MAGICIAN"] },
+  { name: "Hero 17**", tags: ["HUMAN", "FIGHTER", "CLERIC"] },
+  { name: "Hero 18**", tags: ["HUMAN", "MAGICIAN"] },
+  { name: "Hero 19**", tags: ["HUMAN", "FIGHTER"] },
+];
 
 
 const state = (): CardState => ({ cards: [] }) 
 
 const mutations: MutationTree<CardState> = {
-    setCards(state: CardState, cards: Card[]): void {
-      state.cards = cards;
-    }
+  setCards(state: CardState, cards: Card[]): void {
+    state.cards = cards;
+  }
 };
 
-const _sample = (arr: string[]): Card => {
-    const name = arr[Math.floor(Math.random() * arr.length)];
-    return new Card(name, [], "hero");
+
+const _sample = (arr: Candidate[]): Card => {
+  const get_index = Math.floor(Math.random() * arr.length) 
+  const card = arr[get_index];
+  arr.splice(get_index, 1)
+  return new Card(card.name, card.tags, "hero");
 };
 
 const _shuffle = () => {
-    return  [
-        new Card("Hero 1**", ["HUMAN","FIGHTER"], "hero"),
-        new Card("Hero 2**", ["HUMAN","MAGICIAN"], "hero"),
-        new Card("Hero 3**", ["ELF","CLERIC"], "hero"),
-        new Card("Hero 4**", ["HUMAN","FIGHTER","ROAGE"], "hero"),
-    ];
+  let temp_candidates = Array.from(candidates);
+  let cards = []
+  for(let i = 0;  i < 4; i++) {
+      cards.push(_sample(temp_candidates));
+  }
+  return  cards;
 }
 
-const actions: ActionTree<CardState, CardState> = {
-    shuffle({ commit }) {
-        const cards = _shuffle();
-        commit("setCards", cards);
-    },
+const _checkHeroJobs = (cards: Candidate[]) => {
+  let counter: { [key: string]: number} =  {
+    "FIGHTER": 0,
+    "ROAGE": 0,
+    "MAGICIAN": 0,
+    "CLERIC": 0,
+  };
+  for (let card of cards) {
+    for (let jobName in counter) {
+      if(card.tags.includes(jobName)) counter[jobName]++;
+    }
+  }
+
+  // Check: Are all jobs appeared ?
+  for (let jobName in counter) {
+    if(counter[jobName] < 1) return false;
+  }
+  return true;
 };
+
+const actions: ActionTree<CardState, CardState> = {
+  shuffle({ commit }) {
+    let cards: Candidate[] = []
+    let i = 0
+    while(!_checkHeroJobs(cards)) {
+      cards = _shuffle();
+      if(i++ > 100) break; // against for infinity loop
+    }
+    commit("setCards", cards);
+  },
+};
+
 export default {
-    namespace: true,
-    state,
-    mutations,
-    actions,
+  namespace: true,
+  state,
+  mutations,
+  actions,
 };
--- store/marcketplace.ts
+++ store/marcketplace.ts
@@ -11,39 +11,39 @@ const candidate = {
 const state = (): CardState => ({ cards: [] }) 
 
 const mutations: MutationTree<CardState> = {
-    setCards(state: CardState, cards: Card[]): void {
-        state.cards = cards;
-    }
+  setCards(state: CardState, cards: Card[]): void {
+    state.cards = cards;
+  }
 };
 
 const _sample = (arr: string[], card_type: CARD_TYPE): Card => {
-    const get_index = Math.floor(Math.random() * arr.length) 
-    const name = arr[get_index];
-    arr.splice(get_index, 1)
-    return new Card(name, [], card_type);
+  const get_index = Math.floor(Math.random() * arr.length) 
+  const name = arr[get_index];
+  arr.splice(get_index, 1)
+  return new Card(name, [], card_type);
 };
 
 const _shuffle = (card_type: "weapon" | "magic" | "item", draw_num: number) => {
-    let temp_candidate = Array.from(candidate[card_type]);
-    let cards = []
-    for(let i = 0;  i < draw_num; i++) {
-        cards.push(_sample(temp_candidate, card_type));
-    }
-    return  cards;
+  let temp_candidate = Array.from(candidate[card_type]);
+  let cards = []
+  for(let i = 0;  i < draw_num; i++) {
+      cards.push(_sample(temp_candidate, card_type));
+  }
+  return  cards;
 }
 
 const actions: ActionTree<CardState, CardState> = {
-    shuffle({ commit }) {
-        const weapons: Card[] = _shuffle("weapon", 3);
-        const magics: Card[] = _shuffle("magic", 3);
-        const items: Card[] = _shuffle("item", 2);
+  shuffle({ commit }) {
+    const weapons: Card[] = _shuffle("weapon", 3);
+    const magics: Card[] = _shuffle("magic", 3);
+    const items: Card[] = _shuffle("item", 2);
 
-        commit("setCards", [...weapons, ...magics, ...items]);
-    },
+    commit("setCards", [...weapons, ...magics, ...items]);
+  },
 };
 export default {
-    namespace: true,
-    state,
-    mutations,
-    actions,
+  namespace: true,
+  state,
+  mutations,
+  actions,
 };
