4c91a14ffe97803061d9bc85b0d2b75ba1378d28
umanoda <hhenpitu@gmail.com>
2019-09-12 20:54:01 +0900

    marckeht

--- store/marcketplace.ts
+++ store/marcketplace.ts
@@ -2,13 +2,82 @@ import { CardState, Card, CARD_TYPE } from "~/types";
 import { MutationTree, ActionTree } from "vuex";
 
 const candidate = {
-  weapon: ["Short Sword**", "Mace**", "Long Sword**", "Halberd**", "Axe**", "Shortsear**"],
-  magic: ["Fireball**", "Feature**", "Ice Needle**", "Teleport**", "Magic Weapon**"],
-  item: ["Orb A**", "Orb B**", "Orb C**", "Orb D**", "Orb E**", "Orb F**"],
+  weapon: [
+    "Battle Axe",
+    "Boomerang",
+    "Broads word",
+    "Crossbaw",
+    "Crystal Dagger",
+    "Cursed Mace",
+    "Flail",
+    "Hammer",
+    "Hand Axe",
+    "Holy Mace",
+    "King's Sword",
+    "Longbow",
+    "Longspear",
+    "Longsword",
+    "Maggi Staff",
+    "Maul",
+    "Punching Dagger",
+    "Quarterstaff",
+    "Rapier",
+    "Shortbow",
+    "Shortspear ",
+    "Shortsword",
+    "Two-Handed Sword",
+  ],
+  magic: [
+    "Arcane Aura",
+    "Arcane Touch",
+    "Charm Monster",
+    "Consecration",
+    "Creeping Death",
+    "Dark Fire Touch",
+    "Death Pact",
+    "Enchant Weapons",
+    "Fireball",
+    "Form of the Juggernaut",
+    "Frost Bolt",
+    "Future Vision ",
+    "Lightning Bolt",
+    "Magic Missile",
+    "Mind Control",
+    "Mirror Image",
+    "Moonlight",
+    "Nature's Fury",
+    "Summon Storm",
+    "Tempest",
+    "True Seeing",
+    "Vampiric Touch",
+  ],
+  item: [
+    "Amulet of Infravision",
+    "Amulet of Power",
+    "Crystal of Scrying",
+    "Damilu Huskie",
+    "Daramere's Cloak",
+    "Elven Ring",
+    "Gem of Healing",
+    "Headband of Intellect",
+    "Holy Symbol",
+    "Lightstone Gem",
+    "Nature's Amulet",
+    "Necklace of Dawn",
+    "Potion of Light",
+    "Potion of Stamina",
+    "Ring of Learning",
+    "Ring of Proficiency",
+    "Ring of Spell Storing",
+    "Scionic Annals",
+    "Strength Gauntlets",
+    "Tome of Knowledge",
+    "Wand of Light",
+  ]
 };
 
 
-const state = (): CardState => ({ cards: [] }) 
+const state = (): CardState => ({ cards: [] })
 
 const mutations: MutationTree<CardState> = {
   setCards(state: CardState, cards: Card[]): void {
@@ -17,7 +86,7 @@ const mutations: MutationTree<CardState> = {
 };
 
 const _sample = (arr: string[], card_type: CARD_TYPE): Card => {
-  const get_index = Math.floor(Math.random() * arr.length) 
+  const get_index = Math.floor(Math.random() * arr.length)
   const name = arr[get_index];
   arr.splice(get_index, 1)
   return new Card(name, [], card_type);
