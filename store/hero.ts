45d5bf5db3b57fc02d466d8a75d9d679179822de
s.hoshino <hhenpitu@gmail.com>
2019-09-29 15:01:41 +0900

    Prevent multiple cards with the same job from being drawn more than once

--- store/hero.ts
+++ store/hero.ts
@@ -4,40 +4,40 @@ import { MutationTree, ActionTree } from "vuex";
 type Job ='FIGHTER' | 'ROGUE' | 'WIZARD' | 'CLERIC';
 type Candidate = {
   name: string;
-  tags: Job[];
+  jobs: Job[];
   expantion: EXPANTION;
 }
 
 const DECK: Candidate[] = [
   // #1
-  { name: "Grolandor", tags: ["FIGHTER"], expantion: "#1" },
-  { name: "Hawkswood", tags: ["ROGUE"], expantion: "#1" },
-  { name: "Pylorian", tags: ["WIZARD"], expantion: "#1" },
-  { name: "Scathian", tags: ["ROGUE", "WIZARD"], expantion: "#1" },
-  { name: "Silverhelm", tags: ["CLERIC", "FIGHTER"], expantion: "#1" },
-  { name: "Stormhand", tags: ["FIGHTER"], expantion: "#1" },
+  { name: "Grolandor", jobs: ["FIGHTER"], expantion: "#1" },
+  { name: "Hawkswood", jobs: ["ROGUE"], expantion: "#1" },
+  { name: "Pylorian", jobs: ["WIZARD"], expantion: "#1" },
+  { name: "Scathian", jobs: ["ROGUE", "WIZARD"], expantion: "#1" },
+  { name: "Silverhelm", jobs: ["CLERIC", "FIGHTER"], expantion: "#1" },
+  { name: "Stormhand", jobs: ["FIGHTER"], expantion: "#1" },
   // #2
   // #3
-  { name: "Baharan", tags: ["CLERIC"], expantion: "#3" },
-  { name: "Darameric", tags: ["CLERIC", "WIZARD"], expantion: "#3" },
-  { name: "Linsha", tags: [ "FIGHTER"], expantion: "#3" },
-  { name: "Markennan", tags: ["FIGHTER"], expantion: "#3" },
-  { name: "Nimblefingers", tags: ["ROGUE"], expantion: "#3" },
-  { name: "Regalen", tags: ["WIZARD"], expantion: "#3" },
+  { name: "Baharan", jobs: ["CLERIC"], expantion: "#3" },
+  { name: "Darameric", jobs: ["CLERIC", "WIZARD"], expantion: "#3" },
+  { name: "Linsha", jobs: [ "FIGHTER"], expantion: "#3" },
+  { name: "Markennan", jobs: ["FIGHTER"], expantion: "#3" },
+  { name: "Nimblefingers", jobs: ["ROGUE"], expantion: "#3" },
+  { name: "Regalen", jobs: ["WIZARD"], expantion: "#3" },
   // #4
-  { name: "Darkrend", tags: ["WIZARD"], expantion: "#4" },
-  { name: "Grimwolf", tags: ["FIGHTER"], expantion: "#4" },
-  { name: "Honormain", tags: ["CLERIC"], expantion: "#4" },
-  { name: "Jadress", tags: ["ROGUE"], expantion: "#4" },
-  { name: "Moonblades", tags: ["FIGHTER", "ROGUE"], expantion: "#4" },
-  { name: "Stormskull", tags: ["WIZARD"], expantion: "#4" },
+  { name: "Darkrend", jobs: ["WIZARD"], expantion: "#4" },
+  { name: "Grimwolf", jobs: ["FIGHTER"], expantion: "#4" },
+  { name: "Honormain", jobs: ["CLERIC"], expantion: "#4" },
+  { name: "Jadress", jobs: ["ROGUE"], expantion: "#4" },
+  { name: "Moonblades", jobs: ["FIGHTER", "ROGUE"], expantion: "#4" },
+  { name: "Stormskull", jobs: ["WIZARD"], expantion: "#4" },
   // #5
-  { name: "Aird", tags: ["ROGUE"], expantion: "#5" },
-  { name: "Arcanian", tags: ["WIZARD"], expantion: "#5" },
-  { name: "Dunardic", tags: ["FIGHTER"], expantion: "#5" },
-  { name: "Regian", tags: ["CLERIC"], expantion: "#5" },
-  { name: "Terakian", tags: ["CLERIC", "FIGHTER"], expantion: "#5" },
-  { name: "Veris", tags: ["WIZARD"], expantion: "#5" },
+  { name: "Aird", jobs: ["ROGUE"], expantion: "#5" },
+  { name: "Arcanian", jobs: ["WIZARD"], expantion: "#5" },
+  { name: "Dunardic", jobs: ["FIGHTER"], expantion: "#5" },
+  { name: "Regian", jobs: ["CLERIC"], expantion: "#5" },
+  { name: "Terakian", jobs: ["CLERIC", "FIGHTER"], expantion: "#5" },
+  { name: "Veris", jobs: ["WIZARD"], expantion: "#5" },
 ]
 
 const state = (): CardState => ({ cards: [] })
@@ -49,11 +49,33 @@ const mutations: MutationTree<CardState> = {
 };
 
 
-const _sample = (arr: Candidate[]): Card => {
+const _sample = (arr: Candidate[]): [Card, Candidate[]] => {
   const get_index = Math.floor(Math.random() * arr.length)
   const card = arr[get_index];
   arr.splice(get_index, 1)
-  return new Card(card.name, card.tags, "hero", card.expantion);
+  if (!card) {
+    console.error(`Invalid card: ${card}`);
+    return [new Card("---", [], "hero", "#1"), arr];
+  }
+  if (card.jobs.length === 1) {
+    /**
+     * Multiple cards with the same job must not be drawn more than once
+     *
+     * OK:
+     *   FIGHTER, FIGHTER/WIZARD, ROGUE/WIZARD, CLERIC
+     *   FIGHTER/WIZARD, FIGHTER/WIZARD, ROGUE, CLERIC
+     *
+     * NG:
+     *   FIGHTER, ROGUE/WIZARD, FIGHTER, CLERIC
+     *   WIZARD, ROGUE/CLERIC, WIZARD, FIGHTER/CLERIC
+     *
+     * NG but unsupported, because it is a rare case:
+     *   FIGHTER/WIZARD, FIGHTER/WIZARD, FIGHTER/WIZARD, ROGUE/CLERIC
+     */
+    arr = arr.filter(c => c.jobs.toString() !== card.jobs.toString());
+  }
+
+  return [new Card(card.name, card.jobs, "hero", card.expantion), arr];
 };
 
 const _shuffle = (expansionRegexp: RegExp) => {
@@ -63,9 +85,11 @@ const _shuffle = (expansionRegexp: RegExp) => {
     console.error("card is less");
     return cards;
   }
-  let temp_candidates = Array.from(candidates);
+  let nextCandidates = Array.from(candidates);
+  let newCard: Card;
   for (let i = 0; i < 4; i++) {
-    cards.push(_sample(temp_candidates));
+    [newCard, nextCandidates] = _sample(nextCandidates)
+    cards.push(newCard);
   }
   return cards;
 }
