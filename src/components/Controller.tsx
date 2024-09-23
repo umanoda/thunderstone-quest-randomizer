51e65afa09f257171d129a5574559802c377e99b
umanoda <hhenpitu@gmail.com>
2024-09-23 17:12:41 +0900

    feat: impl controller (wip)

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -1,3 +1,93 @@
+import type { ITEM_TYPE } from "../types";
+import { useGame, useGameDispatch } from "./GameContext";
+
+const CardController = (props: {
+  title: string;
+  type?: ITEM_TYPE;
+  numOfCard: number;
+  dispatch?: ReturnType<typeof useGameDispatch>;
+}) => {
+  const { title, type, numOfCard, dispatch } = props;
+
+  return (
+    <div style={{ display: "inline-block", margin: "4px" }}>
+      <div>
+        <span style={{ fontSize: "0.7em", color: "gray", fontWeight: "bold" }}>
+          {title}
+        </span>
+      </div>
+      <div>
+        {dispatch && type ? (
+          <button
+            type="button"
+            onClick={() =>
+              dispatch({
+                type: "change-marketplace",
+                card: type,
+                number: numOfCard - 1,
+              })
+            }
+          >
+            &lt;
+          </button>
+        ) : null}
+        <span
+          style={{ fontSize: "1.2em", paddingLeft: "2px", paddingRight: "2px" }}
+        >
+          {numOfCard}
+        </span>
+        {dispatch && type ? (
+          <button
+            type="button"
+            onClick={() =>
+              dispatch({
+                type: "change-marketplace",
+                card: type,
+                number: numOfCard + 1,
+              })
+            }
+          >
+            &gt;
+          </button>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
 export const Controller = () => {
-  return <div>Controller</div>;
+  const game = useGame();
+  const dispatch = useGameDispatch();
+  return (
+    <div>
+      <div>
+        <CardController
+          title="Monster"
+          type="weapon"
+          numOfCard={game.configMarketplace.numOfWeapons}
+          dispatch={dispatch}
+        />
+        <CardController
+          title="Magic"
+          type="magic"
+          numOfCard={game.configMarketplace.numOfMagics}
+          dispatch={dispatch}
+        />
+        <CardController
+          title="Item"
+          type="item"
+          numOfCard={game.configMarketplace.numOfItems}
+          dispatch={dispatch}
+        />
+        <CardController
+          title="Random"
+          numOfCard={game.configMarketplace.numOfAny}
+        />
+      </div>
+      <div>
+        <button type="submit">Shuffle</button>
+        <button type="button">Copy to clipboard</button>
+      </div>
+    </div>
+  );
 };
--- src/components/GameContext.tsx
+++ src/components/GameContext.tsx
@@ -7,7 +7,12 @@ import {
   useReducer,
 } from "react";
 
-import type { ConfigGame, EXPANSION, ITEM_TYPE } from "../types";
+import type {
+  ConfigGame,
+  ConfigMarketplace,
+  EXPANSION,
+  ITEM_TYPE,
+} from "../types";
 
 type ProviderProp = {
   children: ReactNode;
@@ -58,19 +63,53 @@ const initGameConfig = {
     15: true,
   },
   configMarketplace: {
-    numOfWeapons: 4,
-    numOfMagics: 4,
+    numOfWeapons: 3,
+    numOfMagics: 3,
     numOfItems: 2,
     numOfAny: 0,
   },
 };
 const GameContext = createContext<ConfigGame>(initGameConfig);
 const GameDispatchContext = createContext<Dispatch<Action>>(
-  {} as Dispatch<Action>,
+  {} as Dispatch<Action>
 );
 
 export const useGame = () => useContext(GameContext);
 export const useGameDispatch = () => useContext(GameDispatchContext);
+
+const nextMarketplace = (
+  oldMarketplace: ConfigMarketplace,
+  changeType: ITEM_TYPE,
+  numOfCard: number
+): ConfigMarketplace => {
+  let numOfWeapons = oldMarketplace.numOfWeapons;
+  let numOfMagics = oldMarketplace.numOfMagics;
+  let numOfItems = oldMarketplace.numOfItems;
+  switch (changeType) {
+    case "weapon":
+      numOfWeapons = numOfCard;
+      break;
+    case "magic":
+      numOfMagics = numOfCard;
+      break;
+    case "item":
+      numOfItems = numOfCard;
+      break;
+  }
+  const numOfAny = 8 - numOfWeapons - numOfMagics - numOfItems;
+
+  // 全体の数が8枚を超えるなら更新させない
+  if (numOfAny < 0 || numOfItems < 0 || numOfMagics < 0 || numOfWeapons < 0) {
+    return oldMarketplace;
+  }
+
+  return {
+    numOfWeapons,
+    numOfMagics,
+    numOfItems,
+    numOfAny,
+  };
+};
 
 const gameReducer = (game: ConfigGame, action: Action): ConfigGame => {
   switch (action.type) {
@@ -82,37 +121,44 @@ const gameReducer = (game: ConfigGame, action: Action): ConfigGame => {
               return [key, action.check];
             }
             return [key, v];
-          }),
+          })
         ) as Record<EXPANSION, boolean>,
         configMarketplace: game.configMarketplace,
       };
     case "check-all-expansion":
       return {
         configExpansion: Object.fromEntries(
-          Object.entries(game.configExpansion).map(([key, _]) => [key, true]),
+          Object.entries(game.configExpansion).map(([key, _]) => [key, true])
         ) as Record<EXPANSION, boolean>,
         configMarketplace: game.configMarketplace,
       };
     case "uncheck-all-expansion":
       return {
         configExpansion: Object.fromEntries(
-          Object.entries(game.configExpansion).map(([key, _]) => [key, false]),
+          Object.entries(game.configExpansion).map(([key, _]) => [key, false])
         ) as Record<EXPANSION, boolean>,
         configMarketplace: game.configMarketplace,
       };
     case "change-marketplace":
-      return game;
+      return {
+        configExpansion: game.configExpansion,
+        configMarketplace: nextMarketplace(
+          game.configMarketplace,
+          action.card,
+          action.number
+        ),
+      };
     default:
       return game;
   }
 };
 
 export const GameProvider: FC<ProviderProp> = ({ children }) => {
-  const [game, dispach] = useReducer(gameReducer, initGameConfig);
+  const [game, dispatch] = useReducer(gameReducer, initGameConfig);
 
   return (
     <GameContext.Provider value={game}>
-      <GameDispatchContext.Provider value={dispach}>
+      <GameDispatchContext.Provider value={dispatch}>
         {children}
       </GameDispatchContext.Provider>
     </GameContext.Provider>
